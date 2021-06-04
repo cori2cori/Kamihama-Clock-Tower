a54fc3e76d7ab1e95fa8e7c17da620397992150a
cori2cori <84682305+cori2cori@users.noreply.github.com>
2021-06-04 15:08:36 +0200

    Update data.js

--- data.js
+++ data.js
@@ -291,13 +291,13 @@ var timerData = [
         ],
         "column": 1,
         "priority": 78,
-		keepAfterFinished: "9 hours",
         "timers": [
             {
                 "name": "Duration",
                 "start": "May 24 2021, 16:00",
                 "end": "June 4 2021, 14:59",
-                "extraPriority": 10
+                "extraPriority": 10,
+				keepAfterFinished: "9 hours"
             }
         ]
     },
