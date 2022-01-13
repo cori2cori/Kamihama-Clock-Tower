38b6e175144a47041fa03fec1dff108f89217452
cori2cori <oigres.94@hotmail.com>
2022-01-13 19:41:32 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 var releaseDate = "August 22 2017, 0:00";
-var possibleNextAnnouncementDate = "December 29 2020, 18:00";
+var possibleNextAnnouncementDate = "January 19 2022, 18:00";
 var nextAnniversary = "August 22 2022, 0:00";
 var alertMessages = [];
 var alertTypes = [];
