59edef202bfffcf30da78b2ab200054d94985f8a
cori2cori <84682305+cori2cori@users.noreply.github.com>
2022-10-23 01:52:39 +0200

    Halloween

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 var releaseDate = "August 22 2017, 0:00";
-var possibleNextAnnouncementDate = "Oct 21 2022, 18:00";
+var possibleNextAnnouncementDate = "Nov 4 2022, 18:00";
 var nextAnniversary = "August 22 2023, 0:00";
 var alertMessages = [];
 var alertTypes = [];
@@ -68,26 +68,6 @@ var timerData = [
 			}
 			]
     },
-			{
-        "type": "Maintenance",
-        "title": [
-			"Maintenance",
-			"维护"
-        ],
-        "image": [
-            "imgs/maintenance.png"
-        ],
-        "column": 0,
-        "priority": 11,
-        "timers": [
-			{
-				name: "Maintenance",
-				start: "Sep 26 2022, 15:00",
-				end: "Sep 26 2022, 16:00",
-				extraPriority: 2000
-			}
-        ]
-    },
 		{
         "type": "Event",
         "title": [
@@ -115,183 +95,50 @@ var timerData = [
 			}
         ]
     },
-					{
+{
         "type": "Event",
         "title": [
-			"Kaname Madoka's Birthday Celebration Campaign",
-			"鹿目まどか誕生日記念期間限定キャンペーン"
+			"Dark Halloween is The Colour of Love!? ~ Connect ・The East-West Frontline of Love! ~",
+			"闇色ハロウィンは恋の色!? ～繋げて・恋の東西最前線！～"
         ],
         "image": [
-            "imgs/0210/banner_0624_m.png"
-        ],
-        "column": 2,
-        "priority": 21,
-        "timers": [
-			{
-				name: "Limited-time Memoria in Support Points Shop",
-				start: "Oct 3 2022, 00:00",
-				end: "Oct 11 2022, 14:59",
-				extraPriority: 100
-			},
-			{
-				name: "Limited-time Missions",
-				start: "Oct 3 2022, 00:00",
-				end: "Oct 11 2022, 14:59",
-				extraPriority: 100
-			}
-        ]
-    },
-						{
-        "type": "Event",
-        "title": [
-			"Main Story Arc 2 Chapter 12 Final Part Release & New Chapter Campaign",
-			"メインストーリー第2部12章後編 配信 & 新章キャンペーン"
-        ],
-        "image": [
-            "imgs/2409/banner_1031202_m.png",
-			"imgs/2409/banner_0590_m.png"
-        ],
-        "column": 0,
-        "priority": 9,
-        "timers": [
-			{
-				name: "Campaign duration",
-				start: "Sep 26 2022, 16:00",
-				end: "Oct 3 2022, 14:59",
-				extraPriority: 100
-			}
-        ]
-    },
-	{
-        "type": "Event",
-        "title": [
-			"Closing and Opening Symposium ～Summarizing Arc 2～",
-			"むすんでひらいて座談会～第2部をまとめて～"
-        ],
-        "image": [
-            "imgs/0109/banner_0620_m.png"
+            "imgs/2310/banner_0626_m.png"
         ],
         "column": 0,
         "priority": 10,
         "timers": [
 			{
 				name: "Main Event",
-				start: "Sep 5 2022, 16:00",
-				end: "Sep 16 2022, 14:59",
+				start: "Oct 24 2022, 16:00",
+				end: "Nov 7 2022, 14:59",
 				extraPriority: 100,
-				markers: [{time: "Sep 9 2022, 16:00", title: "Story Quests 11~20 & 100Evils I~III", type: "battle"}]
-			}
-        ]
-    },
-	{
-        "type": "Event",
-        "title": [
-            "Extermination Battle: Witches' Paradox",
-			"殲滅戦 魔女たちのパラドクス"
-        ],
-        "image": [
-            "imgs/extermination.png"
-        ],
-        "column": 0,
-        "priority": 6,
-        "timers": [
-            {
-                "name": "Main Event",
-                "start": "Jul 22 2022, 17:00",
-                "end": "Jul 29 2022, 14:59",
-                "extraPriority": 100,
-				"markers": [
+				markers: [
 				{
-					title: "Challenge Stage",
-					time: "Jul 24 2022, 16:00",
-					type: "battle"
-				},
-				{
-					title: "EX Challenge Stage",
-					time: "Jul 26 2022, 16:00",
-					type: "battle"
+					title: "Story Quests 11-20 & 100Evils Challenges I-III",
+					type: "battle",
+					time: "Oct 31 2022, 16:00"
 				}
 				]
-            }
-        ]
-    },
-	{
-        "type": "Event",
-        "title": [
-			"Ranked Mirrors",
-            "ミラーズランキング"
-        ],
-        "image": [
-            "mirroran"
-        ],
-        "column": 0,
-        "priority": 4,
-        "timers": [
-			{
-				name: "Main Event",
-				start: "Jul 11 2022, 17:00",
-				end: "Jul 17 2022, 20:59",
-				extraPriority: 100
 			}
         ]
-    },
-		{
+   },
+					{
         "type": "Event",
         "title": [
-			"Cinderella Mirrors Tournament",
-            "ミラーズシンデレラトーナメント"
+			"Main Story Arc 2 Special Mission",
+			"メインストーリー第2部特別ミッション"
         ],
         "image": [
-            "imgs/1509/tourny1.png",
-			"imgs/1509/tourny2.png",
+            "imgs/2310/banner_0627_m.png"
         ],
-        "column": 0,
-        "priority": 1,
+        "column": 2,
+        "priority": 21,
         "timers": [
 			{
-				name: "Registration Period",
-				start: "Sep 15 2022, 18:00",
-				end: "Oct 2 2022, 23:59",
-				extraPriority: 0
-			},
-			{
-				name: "Check-in Time",
-				start: "Oct 15 2022, 12:00",
-				end: "Oct 15 2022, 13:00",
-				extraPriority: 50,
-				keepAfterFinished: "3 hours"
-			}
-			
-        ]
-    },
-		{
-        "type": "Event",
-        "title": [
-			"5th Anniversary Program Magia Day 2022",
-            "5周年記念番組「Magia Day 2022」"
-        ],
-        "image": [
-            "imgs/magiaday2022.png"
-        ],
-        "column": 0,
-        "priority": 0,
-        "timers": [
-			{
-				name: "Call for Questions and Concerns",
-				start: "Jul 22 2022, 18:00",
-				end: "Jul 31 2022, 23:59",
+				name: "Complete Arc 2 Chapter 12 to get a x10 gacha ticket",
+				start: "Oct 24 2022, 16:00",
+				end: "Nov 7 2022, 14:59",
 				extraPriority: 100
-			},
-			{
-				name: "Main Event",
-				start: "Sep 17 2022, 20:00",
-				end: "Sep 17 2022, 22:30",
-				extraPriority: 1000
-			},
-			{
-				name: "Archive",
-				start: "Sep 17 2022, 22:30",
-				end: "Sep 30 2022, 23:59"
 			}
         ]
     },
@@ -308,21 +155,6 @@ var timerData = [
         "priority": 100,
         "timers": [
 			{
-				name: "Adjuster Coin Shop: Black - Izumi Kanagi (Eternal Darkness ver.) Gacha",
-				start: "Oct 3 2022, 17:00",
-				end: "Oct 18 2022, 14:59"
-			},
-			{
-				name: "Adjuster Coin Shop: Purple - Dark Attribute Pickup Gacha",
-				start: "Oct 3 2022, 17:00",
-				end: "Oct 18 2022, 14:59"
-			},
-			{
-				name: "Vigorous Opal Neck Kimochi Battle - Medals Shop",
-				start: "Oct 3 2022, 17:00",
-				end: "Oct 17 2022, 14:59"
-			},
-			{
 				name: "Dream Halloween Festa - Event Shop and Missions",
 				start: "Oct 11 2022, 16:00",
 				end: "Oct 31 2022, 14:59",
@@ -337,6 +169,16 @@ var timerData = [
 				name: "Adjuster Coin Shop: Red - Halloween Magical Girl Rerun Gachas",
 				start: "Oct 11 2022, 16:00",
 				end: "Oct 31 2022, 14:59"
+			},
+			{
+				name: "Adjuster Coin Shop: Navy Blue - Mitama (Eternal Darkness ver.) Gacha",
+				start: "Oct 24 2022, 16:00",
+				end: "Nov 14 2022, 14:59"
+			},
+			{
+				name: "Dark Halloween is the Colour of Love!? - Event Shop and Missions",
+				start: "Oct 24 2022, 16:00",
+				end: "Nov 14 2022, 14:59"
 			}
         ]
     },
@@ -403,8 +245,8 @@ var timerData = [
 	{
         type: "Maintenance",
         title: [
-            "Maintenance",
-			"维护"
+            "Maintenance & App ver. 2.5.9",
+			"维护とアプリ版 2.5.9"
         ],
 		"image": [
 			"imgs/maintenance.png"
@@ -414,8 +256,8 @@ var timerData = [
         timers: [
             {
                 name: "Scheduled Time",
-                start: "Oct 11 2022, 15:00",
-                end: "Oct 11 2022, 16:00",
+                start: "Oct 24 2022, 15:00",
+                end: "Oct 24 2022, 16:00",
 				extraPriority: 1000
             }
         ]
@@ -444,20 +286,20 @@ var timerData = [
 	{
         "type": "Gacha",
         "title": [
-			"Limited-time Izumi Kanagi (Eternal Darkness ver.) Pickup Gacha & Step-up Gacha",
-            "期間限定 和泉十七夜 常闇ver. ピックアップガチャとステップアップガチャ"
+			"Limited-time Yakumo Mitama (Eternal Darkness ver.) Pickup Gacha & Step-up Gacha",
+            "期間限定 八雲みたま 常闇ver. ピックアップガチャとステップアップガチャ"
         ],
         "image": [
-            "imgs/0210/gachabanner_1123_m.png",
-			"imgs/0210/gachabanner_1124_m.png"
+            "imgs/2310/gachabanner_1148_m.png",
+			"imgs/2310/gachabanner_1149_m.png"
         ],
         "column": 1,
         "priority": 20,
         "timers": [
             {
                 name: "Duration",
-                start: "Oct 3 2022, 17:00",
-				end: "Oct 11 2022, 14:59",
+                start: "Oct 24 2022, 16:00",
+				end: "Nov 7 2022, 14:59",
                 extraPriority: 100
             }
         ]
