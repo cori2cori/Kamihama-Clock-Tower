099a79aba09ebcb344a83e5a6f16796e957c53c3
cori2cori <84682305+cori2cori@users.noreply.github.com>
2022-11-17 09:50:58 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 var releaseDate = "August 22 2017, 0:00";
-var possibleNextAnnouncementDate = "Nov 23 2022, 18:00";
+var possibleNextAnnouncementDate = "Nov 22 2022, 18:00";
 var nextAnniversary = "August 22 2023, 0:00";
 var alertMessages = [];
 var alertTypes = [];
